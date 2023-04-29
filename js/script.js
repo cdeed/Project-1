b5f8ec91bad2670dc477f41ca97fc0f937523128
Derek <926976+cdeed@users.noreply.github.com>
2023-04-29 17:00:46 -0400

    complete basic requirements

--- js/script.js
+++ js/script.js
@@ -10,19 +10,64 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+let quotes = [
+  {
+    quote: "You have power over your mind - not outside events. Realize this, and you will find strength.",
+    source: "Marcus Aurelius",
+    citation: "Meditations",
+    year: "c. AD 171-175"
+  },
+  {
+    quote: "Dwell on the beauty of life. Watch the stars, and see yourself running with them.",
+    source: "Marcus Aurelius",
+    citation: "Meditations",
+    year: "c. AD 171-175"
+  },
+  {
+    quote: "The happiness of your life depends upon the quality of your thoughts.",
+    source: "Marcus Aurelius",
+    citation: "Meditations",
+    year: "c. AD 171-175"
+  },
+  {
+    quote: "Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth.",
+    source: "Marcus Aurelius",
+    citation: "Meditations",
+    year: "c. AD 171-175"
+  },
+  {
+    quote: "Waste no more time arguing about what a good man should be. Be one.",
+    source: "Marcus Aurelius",
+    citation: "Meditations",
+    year: "c. AD 171-175"
+  }
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote() {
+  return quotes[Math.floor(Math.random()*quotes.length)];
+}
 
 
 /***
  * `printQuote` function
 ***/
+function printQuote() {
+  let quote = getRandomQuote();
 
+  let template = `<p class="quote">${quote.quote}</p><p class="source">${quote.source}`
+
+  if (quote.citation) {
+    template += `<span class="citation">${quote.citation}</span>`;
+  } if (quote.year) {
+    template += `<span class="year">${quote.year}</span>`;
+  }
+
+  document.getElementById('quote-box').innerHTML = (template += "</p>");
+}
 
 
 /***
